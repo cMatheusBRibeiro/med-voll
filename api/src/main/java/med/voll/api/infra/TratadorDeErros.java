54533fe745fa4996434c3ccd82a5408a9cadf239
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-05-30 01:33:26 -0300

    #FEAT: Adicionando tratamento de erro 400

--- api/src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ api/src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +13,20 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErro400::new).toList());
+    }
+
+    private record DadosErro400(String campo, String mensagem) {
+
+        public DadosErro400(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+
     }
 
 }
