100bf4b11c52151cbaf26262e1018f40df189842
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-06-11 20:55:37 -0300

    #FEAT: Adicionando token JWT no sistema e secret por variável de ambiente

--- api/src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ api/src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -2,6 +2,9 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DadosLogin;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DadosTokenJWT;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,12 +21,16 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosLogin dadosLogin) {
-        var token = new UsernamePasswordAuthenticationToken(dadosLogin.login(), dadosLogin.senha());
-        var authentication = this.authenticationManager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dadosLogin.login(), dadosLogin.senha());
+        var authentication = this.authenticationManager.authenticate(authenticationToken);
+        var token = this.tokenService.gerarToken((Usuario) authentication.getPrincipal());
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new DadosTokenJWT(token));
     }
 
 }
