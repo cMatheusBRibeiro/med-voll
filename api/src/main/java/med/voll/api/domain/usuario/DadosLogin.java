9f1809725cecc90af039202a67bd1707a5f508f8
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-06-11 10:49:59 -0300

    #FEAT: Implementando autenticação com BCrypt

---
+++ api/src/main/java/med/voll/api/domain/usuario/DadosLogin.java
@@ -0,0 +1,6 @@
+package med.voll.api.domain.usuario;
+
+public record DadosLogin(
+        String login,
+        String senha
+) { }
--- api/src/main/java/med/voll/api/domain/usuario/Usuario.java
+++ api/src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -2,6 +2,12 @@ package med.voll.api.domain.usuario;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Table(name = "usuarios")
 @Entity(name = "Usuario")
@@ -10,7 +16,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,4 +24,38 @@ public class Usuario {
     private String login;
     private String senha;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
