9765934b58ab0255d5bbf78afe1e069d0fbfb5ce
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-06-21 14:38:43 -0300

    #FEAT: Adicionando alguns exemplos de JsonAlias

--- api/src/main/java/med/voll/api/domain/endereco/DadosEndereco.java
+++ api/src/main/java/med/voll/api/domain/endereco/DadosEndereco.java
@@ -1,28 +1,36 @@
 package med.voll.api.domain.endereco;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record DadosEndereco(
 
     @NotBlank
+    @JsonAlias({"logradouro"})
     String logradouro,
 
     @NotBlank
+    @JsonAlias({"bairro"})
     String bairro,
 
     @NotBlank
     @Pattern(regexp = "\\d{8}")
+    @JsonAlias({"cep"})
     String cep,
 
     @NotBlank
+    @JsonAlias({"cidade"})
     String cidade,
 
     @NotBlank
+    @JsonAlias({"uf", "estado"})
     String uf,
 
+    @JsonAlias({"complemento"})
     String complemento,
 
+    @JsonAlias({"numero"})
     String numero
 
 ) {
--- api/src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
+++ api/src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
@@ -1,5 +1,6 @@
 package med.voll.api.domain.paciente;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.persistence.Embedded;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
@@ -10,20 +11,25 @@ import med.voll.api.domain.endereco.DadosEndereco;
 public record DadosCadastroPaciente(
 
         @NotBlank
+        @JsonAlias({"nome"})
         String nome,
 
         @NotBlank
         @Email
+        @JsonAlias({"email"})
         String email,
 
         @NotBlank
+        @JsonAlias({"telefone"})
         String telefone,
 
         @NotBlank
         @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
+        @JsonAlias({"cpf"})
         String cpf,
 
         @Embedded
         @Valid
+        @JsonAlias({"endereco"})
         DadosEndereco endereco
 ) { }
