2a3a317a13ac83f627fbf9acc82ea08fdfb38f91
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-06-22 00:00:05 -0300

    #FEAT: Adicionando configurações de cabeçalho no SpringDoc

--- api/src/main/java/med/voll/api/infra/springdoc/SpringDocConfigurations.java
+++ api/src/main/java/med/voll/api/infra/springdoc/SpringDocConfigurations.java
@@ -2,6 +2,9 @@ package med.voll.api.infra.springdoc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +15,31 @@ public class SpringDocConfigurations {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI().components(
-            new Components().addSecuritySchemes(
-                "bearer-key",
-                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
-            )
-        );
+                new Components()
+                    .addSecuritySchemes(
+                        "bearer-key",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                    )
+            ).info(
+                new Info()
+                    .title("Voll.med API")
+                    .description(
+                        """
+                        API Rest da aplicação voll.med, contendo as funcionalidades de CRUD de médicos e de 
+                        pacientes, além de agendamento e cancelamentos de consultas.
+                        """
+                    )
+                    .contact(
+                        new Contact()
+                            .name("Time Backend")
+                            .email("backend@voll.med")
+                    )
+                    .license(
+                        new License()
+                            .name("Apache 2.0")
+                            .url("http://voll.med/api/licenca")
+                    )
+            );
     }
 
 }
